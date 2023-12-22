3b03835be8c594921f87cddcc8572d22cfab791f
jj-mac-vm <jarrod@span-eng.com>
2023-12-22 11:04:33 +1000

    removed scrib.py

--- processor/scrib.py
+++
@@ -1,6 +0,0 @@
-consumption_report = "Consumption yesterday: " + str(round(10.5, 1)) + "L \n" 
-+ "Consumption change : " + str(round(11, 1)) + "% \n"
-+" Litres pumped yesterday: " + str(round(54.6, 1)) + "L \n" 
-+ "Level is now: " + str(round(78.91,1)) + "%"
-
-print(consumption_report)
