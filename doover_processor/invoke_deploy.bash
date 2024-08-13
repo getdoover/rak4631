4cea1b51c18beb583590c0e875afb32b1d8ce2ec
jarrod-span-parallels <jarrod@span-eng.com>
2024-08-13 16:55:51 +1000

    updated doover_config

--- doover_processor/invoke_deploy.bash
+++ doover_processor/invoke_deploy.bash
@@ -2,4 +2,4 @@
 
 export PYTHONDONTWRITEBYTECODE=1
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
-python3.11 -m pydoover invoke_local_task on_deploy . --agent 7ef91fcd-9f21-4388-a08d-ba1292142263 --enable-traceback
+python3.11 -m pydoover invoke_local_task on_deploy . --agent 02c3486b-14b8-45c5-9030-f49e4830eab4 --enable-traceback
